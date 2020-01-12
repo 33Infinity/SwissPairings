9833e63214e91f4b38048c74d20420392335f911
33Infinity <40444596+33Infinity@users.noreply.github.com>
2020-01-12 05:00:35 -0800

    Initial Add

---
+++ SwissPairings/SwissPairings/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SwissPairings.Models;
+
+namespace SwissPairings.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
